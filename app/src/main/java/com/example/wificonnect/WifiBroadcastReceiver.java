e0da5091768449b1f1a96d65ed9605e83a165655
PIPOKA Madjé Attia <mpipoka@univ-lome.tg>
2021-03-08 02:16:33 +0100

    firt commit

---
+++ app/src/main/java/com/example/wificonnect/WifiBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.example.wificonnect;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class WifiBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+    }
+}
